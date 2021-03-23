436b5fe01ea5be9db94c6805f13f2cacc193c106
Jaesik Kim <tensaijskim1991@gmail.com>
2021-03-23 18:23:07 +0900

    H2 datasource and console sample

---
+++ spring-boot-h2-sample/src/main/java/io/jay/springbooth2sample/SpringBootH2SampleApplication.java
@@ -0,0 +1,13 @@
+package io.jay.springbooth2sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootH2SampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootH2SampleApplication.class, args);
+	}
+
+}
---
+++ spring-boot-h2-sample/src/test/java/io/jay/springbooth2sample/SpringBootH2SampleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jay.springbooth2sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootH2SampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
